10ce311e8c89424e49e341abb2ad7d1fb87108be
Vadim Constantin <vad.constantin@gmail.com>
2021-11-15 16:37:14 +0100

    trip model good

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_142843) do
+ActiveRecord::Schema.define(version: 2021_11_15_152647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.string "planet"
+    t.string "gravity"
+    t.boolean "exit"
+    t.integer "price_per_night"
+    t.integer "transport_price"
+    t.string "spaceship"
+    t.string "description"
+    t.integer "survival"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
