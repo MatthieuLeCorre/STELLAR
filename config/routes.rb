e4033803ff28a00241321f7824063abc6f81a877
Vadim Constantin <vad.constantin@gmail.com>
2021-11-15 16:16:08 +0100

    add belongs to & many machin to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  ressources :trips, except: '%i[:edit, :delete, :update]' do
-    ressources :bookings, only: '%i[:new, :create]'
-    ressources :dashboard, only: '%i[:show]'
+  resources :trips, except: '%i[:edit, :delete, :update]' do
+    resources :bookings, only: '%i[:new, :create]'
+
   end
+  resources :bookings
+  resources :dashboard, only: '%i[:show]'
 end
