9c685b353ab80cadb48746e659a90214b72dad3d
Jonathan Dubois <dubois.bjonathan@gmail.com>
2021-11-16 15:13:47 +0100

    trips better

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,9 +4,13 @@ class Trip < ApplicationRecord
   validates :price_per_night, presence: true
   validates :transport_price, presence: true
   validates :exit, presence: true
-  validates :spaceship, presence: true
+  validates :spaceship, presence: true, acceptance: { accept: ["Space X Falcon 9", "Space X Falcon Heavy", "Ariane 5",
+                   "NASA Orion Spacecraft", "NASA Atlantis Spacecraft", "Angara",
+                   "Starship SN15", "Proton"]}
   validates :description, presence: true
-  validates :survival, presence: true
+  validates :survival, presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+            less_than_or_equal_to: 100 }
 
   has_many :bookings
   has_many :users, through: :bookings
