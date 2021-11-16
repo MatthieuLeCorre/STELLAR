f6ab4223fa123c9dbc0d2ac4cab8cd9d56f206a5
Vadim Constantin <vad.constantin@gmail.com>
2021-11-16 10:44:28 +0100

    added trips controller

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,35 @@
+class TripsController < ApplicationController
+
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.save
+    redirect_to trips_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def trip_params
+    params.require(:trips).permit(:planet, :gravity, :exit, :price_per_night, :transport_price, :spaceship, :description, :survival)
+  end
+end
